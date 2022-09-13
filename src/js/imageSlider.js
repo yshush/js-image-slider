fd8a853a519253d97a5e7dfae4f09e89a68ec9a8
양성훈 <tjdgns7533@gmail.com>
2022-09-13 16:43:34 +0900

    image-slider

---
+++ src/js/imageSlider.js
@@ -0,0 +1,128 @@
+export default class imageSlider {
+  #currentPosition = 0;
+  #slideNumber = 0;
+  #slideWidth = 0;
+  #intervalId;
+  #autoplay = true;
+  sliderWrapEl;
+  sliderListEl;
+  nextBtnEl;
+  previousBtnEl;
+  indicatorWrapEl;
+  controlWrapEl;
+  constructor() {
+    this.assignElement();
+    this.initSliderNumber();
+    this.initSlideWidth();
+    this.initSlierListWidth();
+    this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
+    this.initAutoplay();
+  }
+  assignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
+    this.controlWrapEl = this.sliderWrapEl.querySelector('#control-wrap');
+  }
+
+  initAutoplay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+  }
+
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  initSlideWidth() {
+    this.#slideWidth = this.sliderListEl.clientWidth;
+  }
+
+  initSlierListWidth() {
+    this.sliderListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+    this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
+  }
+
+  togglePlay(e) {
+    if (e.target.dataset.status === 'play') {
+      this.#autoplay = true;
+      this.controlWrapEl.classList.add('play');
+      this.controlWrapEl.classList.remove('pause');
+      this.initAutoplay();
+    } else if (e.target.dataset.status === 'pause') {
+      this.#autoplay = false;
+      this.controlWrapEl.classList.add('pause');
+      this.controlWrapEl.classList.remove('play');
+      clearInterval(this.#intervalId);
+    }
+  }
+
+  onClickIndicator(e) {
+    const indexPosition = parseInt(e.target.dataset.index, 10);
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#slideWidth * this.#currentPosition
+      }px`;
+      this.setIndicator();
+    }
+  }
+
+  moveToRight() {
+    this.#currentPosition += 1;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+    if (this.#autoplay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
+    this.setIndicator();
+  }
+  moveToLeft() {
+    this.#currentPosition -= 1;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+    if (this.#autoplay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
+    this.setIndicator();
+  }
+
+  createIndicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#slideNumber; i++) {
+      const li = document.createElement('li');
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setIndicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      .classList.add('active');
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/style.css';
+import Slider from './imageSlider';
+
+const slider = new Slider();
